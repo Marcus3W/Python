58c7578b670d10a2efd05dc23a2cc952a33323ea
Marc Wheeler <60431130+Marcus3W@users.noreply.github.com>
2023-09-03 17:33:57 +0100

    Classes and Files completed

---
+++ Codeacademy/Files.py
@@ -0,0 +1,12 @@
+# Project - Hacking The Fender
+
+import csv
+
+compromised_users = []
+
+with open('passwords.csv') as password_file:
+    password_csv = csv.DictReader(password_file)
+
+    for password_row in password_csv:
+        print(password_row['Username'])
+        compromised_users.append(password_row)
